0995b833c5a39bcd170f1ef703794800b33df211
Magda S <220411@student.pwr.wroc.pl>
2018-02-04 23:33:37 +0100

    Initial commit.

---
+++ Grocery/app/src/main/java/com/example/msl/grocery/Grocery.java
@@ -0,0 +1,15 @@
+package com.example.msl.grocery;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.msl.grocery.R;
+
+public class Grocery extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hat_shop);
+    }
+}
